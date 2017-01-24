9b1c65ff8db529a77b8cde3b107e455b770922ad
smitakothari <smita.s.kothari@gmail.com>
2017-01-24 02:18:30 -0600

    Update WebMvcConfigurerAdapter.java

--- src/main/java/com/jlabs/indianrailway/config/WebMvcConfigurerAdapter.java
+++ src/main/java/com/jlabs/indianrailway/config/WebMvcConfigurerAdapter.java
@@ -1,27 +1,27 @@
-// package com.jlabs.indianrailway.config;
+package com.jlabs.indianrailway.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
-// /**
-//  * Created by smita on 1/23/17.
-//  */
-// @Configuration
-// public class WebMvcConfigurerAdapter {
+/**
+ * Created by smita on 1/23/17.
+ */
+@Configuration
+public class WebMvcConfigurerAdapter {
 
-//     @Bean
-//     public org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter forwardToIndex() {
-//         return new org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter() {
-//             @Override
-//             public void addViewControllers(ViewControllerRegistry registry) {
-//                 // forward requests to /admin and /user to their index.html
-//                 registry.addViewController("/pnr").setViewName(
-//                         "forward:/index.html");
-//                 registry.addViewController("/trainStatus").setViewName(
-//                         "forward:/index.html");
-//             }
-//         };
-//     }
+    @Bean
+    public org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter forwardToIndex() {
+        return new org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                // forward requests to /admin and /user to their index.html
+                registry.addViewController("/pnr").setViewName(
+                        "forward:/index.html");
+                registry.addViewController("/trainStatus").setViewName(
+                        "forward:/index.html");
+            }
+        };
+    }
 
-// }
+}
